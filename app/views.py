6358b0ce9c407e3cf9d9b72a55cf796d41a9594e
FeverCode <giddylancs.gl@gmail.com>
2022-06-22 10:52:40 +0300

    Added animation on login and signup page

--- app/views.py
+++ app/views.py
@@ -102,7 +102,7 @@ def edit_profile(request):
 class DealsViewSet(viewsets.ModelViewSet):
     queryset = Deals.objects.all()
     serializer_class = DealsSerializer
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 
 def new_reservation(request):
