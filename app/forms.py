00a8c3380c5c532afb2fd2ff2c8eeaa8067ed0c6
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-06-23 08:40:08 +0300

    style: add margin to save changes btn

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from ast import Pass
 from dataclasses import field
 from pyexpat import model
 from tokenize import Number
+from unittest.util import _MAX_LENGTH
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
@@ -77,21 +78,29 @@ class UpdateUserForm(forms.ModelForm):
                                widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(required=True,
                              widget=forms.TextInput(attrs={'class': 'form-control'}))
-
+    
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username', 'email'  ]
 
 
 class UpdateProfileForm(forms.ModelForm):
     photo = forms.ImageField(widget=forms.FileInput(
         attrs={'class': 'form-control-file'}))
+    phone_number = forms.IntegerField(
+                               required=True,
+                               widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    location = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+
     bio = forms.CharField(widget=forms.Textarea(
         attrs={'class': 'form-control', 'rows': 5}))
+    
 
     class Meta:
         model = Profile
-        fields = ['photo', 'bio']
+        fields = ['photo', 'bio','phone_number','location']
 
 class ReservationForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
